c02026e4ffd5453404999cf0b928a99ae5bfb8d7
jagaither <jgaither@tacc.utexas.edu>
2021-10-21 15:22:06 -0500

    updated shell files to reference publicBuild branches for differently configured default activation method for profiles

--- cloneAndBuildAll.sh
+++ cloneAndBuildAll.sh
@@ -23,6 +23,7 @@ cd ../
 echo cloning and building tapis-bom
 git clone https://github.com/tapis-project/tapis-bom.git
 cd tapis-bom
+git checkout publicBuild
 mvn clean install -DskipTaccProfile
 echo finished building tapis-bom
 
@@ -30,6 +31,7 @@ echo cloning and building tapis-client-java
 cd ../
 git clone https://github.com/tapis-project/tapis-client-java.git
 cd tapis-client-java
+git checkout publicBuild
 mvn clean install -DskipTaccProfile
 echo finished building tapis-client-java
 
@@ -37,6 +39,7 @@ echo cloning and building tapis-shared-java
 cd ../
 git clone https://github.com/tapis-project/tapis-shared-java.git
 cd tapis-shared-java
+git checkout publicBuild
 mvn clean install -DskipTaccProfile
 echo finished building tapis-shared-java
 
@@ -44,6 +47,7 @@ echo cloning and building tapis-java
 cd ../
 git clone https://github.com/tapis-project/tapis-java.git
 cd tapis-java
+git checkout publicBuild
 mvn clean install -DskipTaccProfile
 echo finished building tapis-java
 
@@ -51,6 +55,7 @@ echo cloning and building tapis-systems
 cd ../
 git clone https://github.com/tapis-project/tapis-systems.git
 cd tapis-systems
+git checkout publicBuild
 mvn clean install -DskipTaccProfile
 echo finished building tapis-systems
 
@@ -65,6 +70,7 @@ echo cloning and building tapis-cmd
 cd ../
 git clone https://github.com/tapis-project/tapis-cmd.git
 cd tapis-cmd
+git checkout publicBuild
 mvn clean install -DskipTaccProfile
 mvn -f shaded-pom.xml package -DskipTaccProfile
 echo finished building tapis-cmd
@@ -73,6 +79,7 @@ echo cloning and building tapis-apps
 cd ../
 git clone https://github.com/tapis-project/tapis-apps.git
 cd tapis-apps
+git checkout publicBuild
 mvn clean install -DskipTaccProfile
 echo finished building tapis-apps
 
@@ -81,6 +88,7 @@ echo cloning and building tapis-files
 cd ../
 git clone https://github.com/tapis-project/tapis-files.git
 cd tapis-files
+git checkout publicBuild
 mvn clean install -DskipTaccProfile
 echo finished building tapis-files
 
@@ -88,6 +96,7 @@ echo cloning and building tapis-meta
 cd ../
 git clone https://github.com/tapis-project/tapis-meta.git
 cd tapis-meta
+git checkout publicBuild
 mvn clean install -DskipTaccProfile
 echo finished building tapis-meta
 
@@ -95,6 +104,7 @@ echo cloning and building notifications
 cd ../
 git clone https://github.com/tapis-project/notifications.git
 cd notifications
+git checkout publicBuild
 mvn clean install -DskipTaccProfile
 echo finished building notifications
 
@@ -102,6 +112,7 @@ echo cloning and building tapis-testapps
 cd ../
 git clone https://github.com/tapis-project/tapis-testapps.git
 cd tapis-testapps
+git checkout publicBuild
 mvn clean install -DskipTaccProfile
 echo finished building tapis-testapps
 
